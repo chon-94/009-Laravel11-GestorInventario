4464e5453b055404a2e1b9100157c528ce50539d
chon-94 <107455342+chon-94@users.noreply.github.com>
2025-05-16 15:43:26 -0500

    makeModelMovimiento

---
+++ GI/app/Http/Controllers/MovimientoController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movimiento;
+use Illuminate\Http\Request;
+
+class MovimientoController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Movimiento $movimiento)
+    {
+        //
+    }
+
+    public function edit(Movimiento $movimiento)
+    {
+        //
+    }
+
+    public function update(Request $request, Movimiento $movimiento)
+    {
+        //
+    }
+
+    public function destroy(Movimiento $movimiento)
+    {
+        //
+    }
+}
