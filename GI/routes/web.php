e8f5ae276da242a6ac34bac49c19974371ff1f24
chon-94 <107455342+chon-94@users.noreply.github.com>
2025-05-15 22:28:31 -0500

    ruta

--- GI/routes/web.php
+++ GI/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController; //para poder usar el el modelo productos
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
