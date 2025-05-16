d6f438ade29045fe0ea3c9bfd31d115da8934575
chon-94 <107455342+chon-94@users.noreply.github.com>
2025-05-16 11:44:53 -0500

    ubicandomejor el pie?

--- GI/resources/views/layouts/main.blade.php
+++ GI/resources/views/layouts/main.blade.php
@@ -57,30 +57,31 @@
             <!-- Scripts -->
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-        </div>  
-        
-        <!-- Pie de pagina -->
-        <footer class="bg-white rounded-lg shadow-sm m-4 text-dark dark:opacity-45 dark:bg-red-800">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>chon.
-                </span>
-                <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </footer>
 
+
+        </div>  
+                    <!-- Pie de pagina -->
+        {{-- <footer class="bg-white rounded-lg shadow-sm m-4 text-dark dark:opacity-45 dark:bg-red-800"> --}}
+            <footer class="bg-white border-t border-gray-200 dark:bg-red-900 dark:border-red-700 shadow-inner">
+                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>chon.
+                    </span>
+                    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
     </body>
 
 </html>
