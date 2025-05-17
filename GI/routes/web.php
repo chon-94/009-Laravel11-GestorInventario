23424ad0815d27839cf7368b268382c9712cf00a
chon-94 <107455342+chon-94@users.noreply.github.com>
2025-05-17 13:20:09 -0500

    vistas

--- GI/routes/web.php
+++ GI/routes/web.php
@@ -11,10 +11,8 @@ Route::get('/', function () {
 Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
 Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos.create');
 Route::post('productos', [ProductoController::class, 'store'])->name('productos.store');
-
 Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
-
 Route::get('/productos/{id}/edit', [ProductoController::class, 'edit'])->name('productos.edit');
 Route::put('/productos/{id}/update', [ProductoController::class, 'update'])->name('productos.update');
-
 Route::delete('/productos/{id}/destroy', [ProductoController::class, 'destroy'])->name('productos.destroy');
+
