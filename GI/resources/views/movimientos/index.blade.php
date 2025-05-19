8f5ac7a2082e15bfa2ba1fa896061bfa4b3064b9
chon-94 <107455342+chon-94@users.noreply.github.com>
2025-05-19 01:43:31 -0500

    boton vista a crear

--- GI/resources/views/movimientos/index.blade.php
+++ GI/resources/views/movimientos/index.blade.php
@@ -73,12 +73,13 @@
                     {{-- @endforelse --}}
                 </div>
             </div>
+        
         </div>
 
         <!-- Botón para registrar nuevo movimiento -->
         <div class="mt-6 flex justify-end">
             {{-- <a href="{{ route('movimientos.create') }}"  --}}
-            <a href="#" 
+            <a href="{{ route('movimientos.create') }}" 
                class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors">
                 Registrar Nuevo Movimiento
             </a>
